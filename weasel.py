d24a37f9dcb91c23f933a8f21fbe1a7ebddd3144
Mark Aleksander Hil <mark@100shapes.com>
2019-02-05 11:53:42 +0000

    Random letter function

--- weasel.py
+++ weasel.py
@@ -2,25 +2,33 @@ import string
 from random import randint
 alphabet = string.ascii_lowercase + " "
 
+def random_letter():
+    alphabet = string.ascii_lowercase + " "
+    rand_letter = alphabet[randint(0, len(alphabet)-1)]
+    return rand_letter
+
+
 def create_initial(target):
     # REFACTOR
     length = len(target)
     initial = []
     for i in range(length):
-        initial.append(alphabet[randint(0, len(alphabet)-1)])
+        initial.append(random_letter())
     return ''.join(initial)
 
-def mutate(str, chance):
+def mutate(str, chance, target):
     # REFACTOR
-    # If character is the same as character in same position of target string, do not change
     str = list(str)
     mutated = []
-    for i in str:
-        random = randint(0, 100)
-        if random < 5:
-            mutated.append('!')
+    for i in range(len(str)):
+        if str[i] == target[i]:
+            mutated.append(str[i])
         else:
-            mutated.append(i)
+            random = randint(0, 100)
+            if random < chance:
+                mutated.append(random_letter())
+            else:
+                mutated.append(str[i])
     return ''.join(mutated)
 
 def generation():
@@ -33,4 +41,5 @@ if __name__ == '__main__':
     target = "some target string"
     chance =  5
     initial = create_initial(target)
-    print(mutate(initial, chance))
+    for i in range(10):
+        print(mutate(initial, chance, target))
